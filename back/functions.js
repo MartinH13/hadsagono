e50b2a101d1e44c1446009ed8e970f964e2d8d69
MartinH13 <martin.i.horsfield@gmail.com>
2024-04-23 10:31:59 +0200

    matriz de adyacencia a pelo

--- back/functions.js
+++ back/functions.js
@@ -10,23 +10,37 @@ class Functions {
     static possibleMoves = {
         "0,0" : [m.e, m.s],
         "0,1" : [m.n, m.ne, m.e, m.s],
-        
+        "0,2" : [m.n, m.ne, m.e, m.s],
+        "0,3" : [m.n, m.ne, m.e, m.s],
+        "0,4" : [m.n, m.ne, m.e, m.s],
         "0,5" : [m.n, m.ne],
 
         "1,0" : [m.e, m.se, m.s, m.sw, m.w],
         "1,1" : [m.n, m.e, m.se, m.s, m.sw, m.w],
-
+        "1,2" : [m.n, m.e, m.se, m.s, m.sw, m.w],
+        "1,3" : [m.n, m.e, m.se, m.s, m.sw, m.w],
         "1,4" : [m.n, m.e, m.se, m.sw, m.w],
 
         "2,0" : [m.e, m.s, m.w],
         "2,1" : [m.n, m.ne, m.e, m.s, m.w, m.nw],
+        "2,2" : [m.n, m.ne, m.e, m.s, m.w, m.nw],
+        "2,3" : [m.n, m.ne, m.e, m.s, m.w, m.nw],
+        "2,4" : [m.n, m.ne, m.e, m.s, m.w, m.nw],
+        "2,5" : [m.n, m.ne, m.nw],
 
-        "2,5" : [m,n, m.ne, m.nw],
+        "3,0" : [m.e, m.se, m.s, m.sw, m.w],
+        "3,1" : [m.n, m.e, m.se, m.s, m.sw, m.w],
+        "3,2" : [m.n, m.e, m.se, m.s, m.sw, m.w],
+        "3,3" : [m.n, m.e, m.se, m.s, m.sw, m.w],
+        "3,4" : [m.n, m.e, m.se, m.sw, m.w],
 
         "4,0" : [m.s, m.w],
         "4,1" : [m.n, m.s, m.w, m.nw],
-
+        "4,2" : [m.n, m.s, m.w, m.nw],
+        "4,3" : [m.n, m.s, m.w, m.nw],
+        "4,4" : [m.n, m.s, m.w, m.nw],
         "4,5" : [m.n, m.nw]
+
     }
 
     static m = {
