02c75bf2a83b9ceaeee7ae67d418b55a3fe5ed4e
Asier Contreras <asicontre@gmail.com>
2024-04-30 13:20:42 +0200

    el fondo baila como shakira cuando fallas

--- front/src/Board.jsx
+++ front/src/Board.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import './Board.css';
 
 
+
+
 const Board = () => {
   const boardData = {
     "board": [
@@ -13,9 +15,15 @@ const Board = () => {
       [16, -1, 8, -1, 16]
     ]
   };
+  
+  const score = {
+    "score": 15
+  };
+
 
   const [selectedHexagons, setSelectedHexagons] = useState([]);
   const [isMouseDown, setIsMouseDown] = useState(false);
+  const [applyStyle, setApplyStyle] = useState(false);
 
   const getHexagonColor = (value) => {
     const colors = [
@@ -41,15 +49,15 @@ const Board = () => {
   const handleMouseDown = (rowIndex, colIndex) => {
     const hexagon = boardData.board[rowIndex][colIndex];
     if (hexagon !== -1 && !selectedHexagons.some(hexagon => hexagon.row === rowIndex && hexagon.col === colIndex)) {
+      setApplyStyle(false);
       setSelectedHexagons([{ row: rowIndex, col: colIndex, value: hexagon, selected: true }]);
       setIsMouseDown(true);
     }
   };
-
   const handleMouseEnter = (rowIndex, colIndex) => {
     if (isMouseDown) {
       const hexagon = boardData.board[rowIndex][colIndex];
-      if (hexagon !== -1 ){
+      if (hexagon !== -1) {
         setSelectedHexagons(prevSelectedHexagons => {
           const currentIndex = prevSelectedHexagons.findIndex(
             hexagon => hexagon.row === rowIndex && hexagon.col === colIndex
@@ -58,10 +66,34 @@ const Board = () => {
             // Remove the last hexagon from the list
             return prevSelectedHexagons.slice(0, prevSelectedHexagons.length - 1);
           } else {
-            return [
-              ...prevSelectedHexagons,
-              { row: rowIndex, col: colIndex, value: hexagon, selected: true }
-            ];
+            // Check if it's the first selected hexagon
+            if (prevSelectedHexagons.length === 0) {
+              return [
+                ...prevSelectedHexagons,
+                { row: rowIndex, col: colIndex, value: hexagon, selected: true }
+              ];
+            }
+            // Check if the previous hexagon has the same value as the first selected hexagon
+            else if (
+              prevSelectedHexagons.length > 0 &&
+              prevSelectedHexagons[0].value === hexagon
+            ) {
+              return [
+                ...prevSelectedHexagons,
+                { row: rowIndex, col: colIndex, value: hexagon, selected: true }
+              ];
+            }
+            // If the previous hexagon doesn't have the same value as the first selected hexagon, don't add it to the list
+            else {
+              //DO SOME CSS CHANGE HERE
+              
+              setTimeout(() => setApplyStyle(true), 100);
+              setTimeout(() => setApplyStyle(false), 700); // Reset applyStyle after 1 second
+
+              setSelectedHexagons([]);
+              return prevSelectedHexagons;
+              
+            }
           }
         });
       }
@@ -69,15 +101,21 @@ const Board = () => {
   };
 
   const handleMouseUp = () => {
+    if(selectedHexagons.length <3){
+      setTimeout(() => setApplyStyle(true), 100); // Start shaking after 1 second
+      setTimeout(() => setApplyStyle(false), 700); // Reset applyStyle after 1 second
+      setSelectedHexagons([]);
+    
+    }
     setIsMouseDown(false);
-    setSelectedHexagons([]);
   };
 
   return (
    
-    <div className="board" onMouseLeave={handleMouseUp}>
+    <div className="board" >
       <h1 className='title' >HADSAGONO</h1>
-      <div className='container'>
+      <h3>SCORE: {score.score}</h3>
+      <div className={`container ${applyStyle ? 'shake' : ''}`} style={applyStyle ? { backgroundColor : '#DE7676' } : {}}>
       {boardData.board.map((row, rowIndex) => (
         
         <div key={rowIndex} className="board-row" >
